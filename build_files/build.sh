fa9953cad72a925faf4fe4844da54ad9c4f58658
gbrsni <72036069+gbrsni@users.noreply.github.com>
2025-07-14 13:32:37 +0200

    Don't install NVIDIA

--- build_files/build.sh
+++ build_files/build.sh
@@ -61,31 +61,31 @@ dnf5 remove -y \
 	gnome-software-rpm-ostree
 
 
-# NVIDIA
-dnf5 install -y \
-	akmod-nvidia \
-	xorg-x11-drv-nvidia-cuda \
-	# xorg-x11-drv-nvidia \
-	# xorg-x11-drv-nvidia-cuda \
-	# xorg-x11-drv-nvidia-cuda-libs
+# # NVIDIA
+# dnf5 install -y \
+# 	akmod-nvidia \
+# 	xorg-x11-drv-nvidia-cuda \
+# 	# xorg-x11-drv-nvidia \
+# 	# xorg-x11-drv-nvidia-cuda \
+# 	# xorg-x11-drv-nvidia-cuda-libs
 
-KERNEL_SUFFIX=""
+# KERNEL_SUFFIX=""
 
-QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
-/usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
-chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+# QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+# /usr/bin/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+# chmod 0600 "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
 
-dnf install -y \
-	-x libva-nvidia-driver.i686 \
-	libva-nvidia-driver \
-	libva-utils \
-	nvidia-vaapi-driver
+# dnf install -y \
+# 	-x libva-nvidia-driver.i686 \
+# 	libva-nvidia-driver \
+# 	libva-utils \
+# 	nvidia-vaapi-driver
 
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo | \
 	sudo tee /etc/yum.repos.d/nvidia-container-toolkit.repo
 
-dnf5 install -y \
-	nvidia-container-toolkit
+# dnf5 install -y \
+# 	nvidia-container-toolkit
 
 # nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml # TODO: Make this a service that runs on login
 
