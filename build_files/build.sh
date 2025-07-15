02d59fde003f9fd55b27373156455f3343813152
gbrsni <72036069+gbrsni@users.noreply.github.com>
2025-07-15 21:02:30 +0200

    Remove stale comments

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,16 +2,6 @@
 
 set -ouex pipefail
 
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-# dnf5 install -y tmux 
-
 
 # RPMFusion
 dnf5 install -y \
@@ -116,25 +106,3 @@ systemctl enable docker
 systemctl enable libvirtd.service
 systemctl enable rpm-ostreed-automatic.timer
 sed -i 's/none/stage/g' /etc/rpm-ostreed.conf
-
-
-# tee /etc/systemd/system/rpm-ostreed-automatic.timer.d/override.conf << EOF
-# [Timer]
-# OnBootSec=
-# OnUnitInactiveSec=
-# OnCalendar=Mon *-*-* 18:00:00
-# Persistent=true
-# RandomizedDelaySec=15m
-# EOF
-
-
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
-
-#### Example for enabling a System Unit File
-
-# systemctl enable podman.socket
